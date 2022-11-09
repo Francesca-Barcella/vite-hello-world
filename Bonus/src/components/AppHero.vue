a2eb5ca32b87e4cdfe9a4174374a5f7170d6f479
Francesca-Barcella <pollon.fra@gmail>
2022-11-09 20:17:39 +0100

    vite-hello-word - cartella bonus per prove

---
+++ Bonus/src/components/AppHero.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'AppHero'
+}
+</script>
+
+<template>
+  <div>
+    <img src="../assets/img/jumbo-bg.png" alt="">
+  </div>
+
+</template>
+
+<style scoped>
+img {
+    max-width: 100%;
+    margin: auto;
+}
+</style>
---
+++ Bonus/src/components/AppTitle.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: 'AppTitle'
+}
+</script>
+
+<template>
+  <h1>la mia prima app con Vite</h1>
+</template>
+
+<style scoped>
+  h1 {
+    font-size: 2rem;
+    line-height: 2;
+    color: #2f2f2f;
+    background-color: #f0f0f090;
+    padding: 2rem;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    margin: auto;
+  }
+</style>
