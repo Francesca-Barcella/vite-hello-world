fe1fb4202affc8cf447f6086c428b7e115728b8e
Francesca-Barcella <pollon.fra@gmail>
2022-11-09 16:48:33 +0100

    vite-hello-wordl - bonus initial commit

---
+++ Bonus/src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'AppTitle'
+}
+</script>
+
+<template>
+  <h1>la mia prima app con Vite</h1>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 2em;
+  line-height: 1.1;
+  color: red;
+}
+</style>
