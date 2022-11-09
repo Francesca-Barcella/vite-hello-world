b02a35d371df05b8718dfebc24b9101db2ce9cdb
Francesca-Barcella <pollon.fra@gmail>
2022-11-09 20:42:26 +0100

    vite-hello-world work-in-progress

---
+++ bonus-2/src/App.vue
@@ -0,0 +1,24 @@
+<script>
+
+import AppTitleHero from './components/AppTitleHero.vue';
+
+export default {
+
+  components: {
+    AppTitleHero
+  }
+
+}
+
+</script>
+
+<template>
+  
+  <AppTitleHero></AppTitleHero>
+ 
+
+</template>
+
+<style>
+
+</style>
---
+++ bonus-2/src/components/AppTitleHero.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  name: 'AppTitleHero'
+}
+</script>
+
+<template>
+  <div>
+    <h1>la mia prima app con Vite</h1>
+  </div>
+</template>
+
+<style scoped>
+  div{
+    background-image: url("../assets/img/jumbo-bg.png");
+    background-size: cover;
+    height: 100vh;
+    width: 100vw;
+  }
+  
+h1 {
+  font-size: 2rem;
+  line-height: 2;
+  color: #2f2f2f;
+  background-color: #f0f0f090;
+  padding: 2rem;
+}
+
+
+</style>
+
+
+
+
+<!-- 
+img {
+  max-width: 100%;
+  margin: auto;
+} -->
+<!-- <img src="../assets/img/jumbo-bg.png" alt=""></img> -->
